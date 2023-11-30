a25959d2f65ab45fd8341ea12bdbd79ee778c5f2
Shane Dombowsky <thebigdombowsky@gmail.com>
2023-11-30 10:50:02 -0700

    SD-Add comments

--- src/tests/create-new-orders-test.js
+++ src/tests/create-new-orders-test.js
@@ -1,3 +1,4 @@
+//Define imports
 import { sleep, check } from "k6";
 import http from "k6/http";
 import { uuidv4, weighted_random } from "../utils.js";
@@ -8,10 +9,19 @@ import {
   productInfoMap,
   weights,
 } from "../productInfoMap.js";
+
+// Add a new trend to track the duration of the receiveHISOrders transaction in k6
 export const receiveHISOrdersTrend = new Trend("receiveHISOrders_duration");
 
+//Define variables for use in the createNewOrders function
 let selected_item, product, sender, receiver, upc;
+
 export default function createNewOrders(orderType) {
+
+   //This code is used to select a random product id from a list of product ids.
+  //The product ids are weighted according to the order type.
+  //The product ids are selected according to the order type.
+  //The selected product id is returned.
   if (orderType === "AUTO") {
     selected_item = weighted_random(automationProductIds, weights);
     console.log(selected_item);
@@ -20,6 +30,8 @@ export default function createNewOrders(orderType) {
     console.log(selected_item);
   }
 
+  //The selected product id is used to determine the product, sender, receiver, and upc.
+  //The product, sender, receiver, and upc are used to create the XML message.
   if (productInfoMap[selected_item]) {
     product = productInfoMap[selected_item].product;
     sender = productInfoMap[selected_item].sender;
@@ -29,15 +41,19 @@ export default function createNewOrders(orderType) {
     console.log(`Order type ${orderType} not found.`);
   }
 
-  const url =
-    "http://172.24.220.177:16384/IncomingWebServiceImpl/IncomingWebService";
-  const isoString = new Date().toISOString();
+  // Define the URL to send the HTTP request to
+  const url = "http://172.24.220.177:16384/IncomingWebServiceImpl/IncomingWebService";
+  // Define the current time in ISO format;
   const msgTime = isoString.slice(0, -1);
+  // Define the quantity of the product to be ordered
   const quantity = "1";
+  // Define the UUID of the message
   const msgUUID = uuidv4();
 
+// Set the environment variable WM6_ORDER_ID to the UUID of the message
   __ENV.WM6_ORDER_ID = msgUUID;
 
+  // Define the XML message to be sent to the MedPortal
   const pharmacyOrderMsg = `<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:host="http://host.wm6.swisslog.com/">
        <soapenv:Header/>
        <soapenv:Body>
@@ -83,18 +99,20 @@ export default function createNewOrders(orderType) {
     </soapenv:Envelope>
     `;
 
-  // 1. Save the current time
+  // Save the current time
   const startTime = Date.now();
-  // 2. Send the HTTP request
+  // Send the HTTP request
   const res = http.post(url, pharmacyOrderMsg, {
     headers: { "Content-Type": "text/xml" },
   });
-  // 3. Save the time after the request completes
+  // Save the time after the request completes
   const endTime = Date.now();
+  // Log the duration of the request
   console.log(
     `Call to pharmacyOrders took ${endTime - startTime} milliseconds`,
   );
 
+// Log the HTTP response times for each stage of the request
   const timings = res.timings;
   console.log(`Blocked time: ${timings.blocked} ms`); // Time spent in a queue waiting for a network connection
   console.log(`Connecting time: ${timings.connecting} ms`); // Time spent setting up TCP connection
@@ -104,15 +122,17 @@ export default function createNewOrders(orderType) {
   console.log(`Receiving time: ${timings.receiving} ms`); // Time spent receiving the response
   console.log(`Duration: ${timings.duration} ms`); // Total duration of the request
 
+  // Check that the response code is 200 and that the transaction time is less than 2 seconds
   {
     check(res, {
       "status is 200": (r) => r.status === 200,
       "transaction time OK": (r) => r.timings.duration < 2000,
     });
-
+// Add the duration of the receiveHISOrders transaction to the receiveHISOrdersTrend trend
     receiveHISOrdersTrend.add(res.timings.duration);
     sleep(1);
 
+// Return the UUID of the message and the selected item
     return [msgUUID, selected_item];
   }
 }
--- src/tests/transactions-test.js
+++ src/tests/transactions-test.js
@@ -1,3 +1,9 @@
+
+// This code creates a new order, and then picks the new order
+// The order type is determined by the value passed into the createNewOrders function
+// The order type is passed into the pickNewOrders function to determine which order to pick
+// The order type is also passed into the createNewOrders function to determine which order to create
+
 import { group } from "k6";
 import createNewOrders from "./create-new-orders-test.js";
 import pickNewOrders from "./pick-new-orders-test.js";
@@ -5,16 +11,14 @@ import { executeWithCustomProbability } from "../utils.js";
 
 let orderInfo = {};
 export default function () {
-  const typeOfOrder = executeWithCustomProbability(80); // Change this value as needed
+  const typeOfOrder = executeWithCustomProbability(80); // Value passed in represents a % chance that an outcome will be selected
+  //In this case there is an 80% chane that the order will be of type AUTO. Change this value as needed
 
-  // Group requests or other test actions here
   group("createHISOrder", function () {
     orderInfo = createNewOrders(typeOfOrder);
-    // Additional actions related to creating orders
   });
 
   group("pickNewOrders", function () {
     pickNewOrders(orderInfo[0], orderInfo[1]);
-    // Additional actions related to picking new orders
   });
 }
